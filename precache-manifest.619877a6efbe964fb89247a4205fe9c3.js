62c409f0ccbac2650dc3a76d0529e7e77c99c790
h-Benitez13 <hbenitez3711@gmail.com>
2019-07-10 16:02:55 -0400

    Updates

---
+++ precache-manifest.619877a6efbe964fb89247a4205fe9c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421c397c328cd129ccf7bdbe8ebb7991",
+    "url": "/Blackartistparty/index.html"
+  },
+  {
+    "revision": "218a240b81f156f51d6b",
+    "url": "/Blackartistparty/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c28d066fa069b57aa206",
+    "url": "/Blackartistparty/static/js/2.fd25479e.chunk.js"
+  },
+  {
+    "revision": "218a240b81f156f51d6b",
+    "url": "/Blackartistparty/static/js/main.55018acc.chunk.js"
+  },
+  {
+    "revision": "1b6f8cfd43f9f3cf8668",
+    "url": "/Blackartistparty/static/js/runtime~main.2f701cdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Blackartistparty/static/media/logo.5d5d9eef.svg"
+  }
+]);
