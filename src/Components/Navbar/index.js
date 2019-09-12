90a2f81e79832b69f81fab604b26f50fdb2f9d07
h-Benitez13 <hbenitez3711@gmail.com>
2019-09-12 10:34:34 -0400

    pdf version beta

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,16 +3,22 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="dt w-100 pa3">
-      <Link className="dtc black hover-blue no-underline grow" to="/home">
-        Home
+    <nav className="dt tc mw7 center ma4 pa-3">
+      <Link className="f5-l black bg-animate black-80 dib pa3 ph4-l no-underline" to="/home">
+        <h1 className="f3 fw3">HOME</h1>
       </Link>
-      <Link className="dtc black hover-blue no-underline grow" to="/shop">
-        Shop
+      <Link className="f5-l black bg-animate black-80 dib pa3 ph4-l no-underline" to="/shop">
+        <h1 className="f3 fw3">SHOP</h1>
       </Link>
-      <div className="dtc hover-blue no-underline grow">Press</div>
-      <div className="dtc hover-blue no-underline grow">Gallery</div>
-      <div className="dtc hover-blue no-underline grow">Work</div>
-    </div>
+      <div className="f5-l black bg-animate black-80 dib pa3 ph4-l no-underline">
+        <h1 className="f3 fw3"> PRESS</h1>
+      </div>
+      <div className="f5-l black bg-animate black-80 dib pa3 ph4-l no-underline">
+        <h1 className="f3 fw3">GALLERY</h1>
+      </div>
+      <div className="f5-l black bg-animate black-80 dib pa3 ph4-l no-underline">
+        <h1 className="f3 fw3">WORK</h1>
+      </div>
+    </nav>
   );
 };
--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -8,19 +8,12 @@ function Home() {
     <div className="home">
       <Navbar />
       <div className="container">
-        <iframe
-          title="youtube test"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/CVO8gjn3dFg?&autoplay=1&controls=0&amp;start=133"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-        <div className="overlay">
-          <img className="bap" src={BAP} alt="true logo" />
+        <div className="landing-img">
+          <img src={BAP} alt="BlackartistParty logo" />
         </div>
+
       </div>
+
     </div>
   );
 }
